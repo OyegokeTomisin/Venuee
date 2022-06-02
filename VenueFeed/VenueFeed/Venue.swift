1b5d846a0f9a3e0f37caf93111647648c1202b1f
tomisin <tomisin18@gmail.com>
2022-06-02 09:59:08 +0100

    Create venue framework

---
+++ VenueFeed/VenueFeed/Venue.swift
@@ -0,0 +1,8 @@
+//
+//  Venue.swift
+//  VenueFeed
+//
+//  Created by Oyegoke Oluwatomisin on 02/06/2022.
+//
+
+import Foundation
---
+++ VenueFeed/VenueFeed/VenueFeed.h
@@ -0,0 +1,18 @@
+//
+//  VenueFeed.h
+//  VenueFeed
+//
+//  Created by Oyegoke Oluwatomisin on 02/06/2022.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for VenueFeed.
+FOUNDATION_EXPORT double VenueFeedVersionNumber;
+
+//! Project version string for VenueFeed.
+FOUNDATION_EXPORT const unsigned char VenueFeedVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <VenueFeed/PublicHeader.h>
+
+
