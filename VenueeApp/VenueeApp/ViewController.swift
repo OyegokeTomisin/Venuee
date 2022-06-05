000678e169015e0be299fee913dc23ecb3a91146
tomisin <tomisin18@gmail.com>
2022-06-05 21:25:53 +0100

    Add main project workspace

---
+++ VenueeApp/VenueeApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  VenueeApp
+//
+//  Created by Oyegoke Oluwatomisin on 05/06/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        view.backgroundColor = .red
+    }
+
+
+}
+
